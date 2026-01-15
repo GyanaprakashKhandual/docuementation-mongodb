e04ba65ed01963ccecce64f0a4e0433c704388f9
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2026-01-15 19:21:19 +0530

    Search UI added #234590

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,24 @@
-import { getMarkdownContent } from '@/app/lib/mark.down'
-import MarkdownRenderer from '@/app/components/Mark.down.render'
-import RightSidebar from '@/app/components/Right.sidebar'
+import { getMarkdownContent } from "@/app/lib/mark.down";
+import MarkdownRenderer from "@/app/components/Mark.down.render";
+import RightSidebar from "@/app/components/Right.sidebar";
+import SearchBar from "./components/Search.bar";
 
 export default function Home() {
-  const markdownData = getMarkdownContent('basic', 'Home')
+  const markdownData = getMarkdownContent("basic", "Home");
 
   return (
-    <div className="flex flex-1 min-h-screen">
-      <div className="flex-1 max-w-4xl mx-auto px-6 py-8 md:px-8 md:py-10 lg:px-12 lg:py-12">
-        {markdownData && <MarkdownRenderer content={markdownData.content} />}
-      </div>
+    <>
+      <SearchBar markdownData={markdownData} />
 
-      {markdownData && <RightSidebar content={markdownData.content} />}
-    </div>
-  )
+      <div className="flex flex-1 min-h-screen">
+        <div className="flex-1 max-w-4xl mx-auto px-6 py-8 md:px-8 md:py-10 lg:px-12 lg:py-12">
+          {markdownData && (
+            <MarkdownRenderer content={markdownData.content} />
+          )}
+        </div>
+
+        {markdownData && <RightSidebar content={markdownData.content} />}
+      </div>
+    </>
+  );
 }
